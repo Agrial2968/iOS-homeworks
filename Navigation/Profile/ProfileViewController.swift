4eb0e3cee3dd002748e2436f83875ee8780b7823
Agrial <alexandrovger@yandex.ru>
2024-06-21 12:47:29 +0300

    zadacha 2.1

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    let profileHeaderView = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        
+        view.addSubview(profileHeaderView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        profileHeaderView.frame = view.bounds
     }
 }
