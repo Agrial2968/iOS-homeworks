2f76ba3bd4df5e98db56610aea76114160aaf1cc
Agrial <alexandrovger@yandex.ru>
2024-06-22 16:35:06 +0300

    zadacha 2.2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,14 +11,37 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     let profileHeaderView = ProfileHeaderView()
+    let button = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
+        setupUI()
     }
     
-    override func viewWillLayoutSubviews() {
-        profileHeaderView.frame = view.bounds
+    func setupUI() {
+        view.backgroundColor = .lightGray
+        
+        view.addSubview(profileHeaderView)
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Knopka", for: .normal)
+        
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            profileHeaderView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            profileHeaderView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            button.leftAnchor.constraint(equalTo: view.leftAnchor),
+            button.rightAnchor.constraint(equalTo: view.rightAnchor),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            
+            
+        ])
+        
+               
     }
 }
